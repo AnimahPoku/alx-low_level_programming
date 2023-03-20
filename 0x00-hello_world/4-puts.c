2a4fe78aebebb0c4ded7922e488646eb8e3062be
AnimahPoku <animahpoku@gmail.com>
2023-03-19 19:11:58 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main-a program that prints a line with the puts function
- * should return :0 sucess
+ * Return:0
  */
 int main(void)
 {
 puts("/"Programming is like building a multilingual puzzle");
-return(0)
+return(0);
 }
 
